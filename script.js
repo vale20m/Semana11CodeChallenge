6864df8ae71e89444ef5f962eb71c9f176cd8192
Vale20m <valentinveintemilla@gmail.com>
2023-09-04 21:14:44 -0300

    Arreglos en el codigo

--- script.js
+++ script.js
@@ -1,23 +1,19 @@
 const URL = "https://jsonplaceholder.typicode.com/users";
 
 const formulario = document.querySelector("#formulario");
-const enviar = document.querySelector("#enviar");
-const resultado = document.querySelector("#resultado");
 
-enviar.addEventListener("click", function(e){
-    e.preventDefault;
-    const contForm = new FormData(formulario);
-    for (const [key, value] of contForm) {
-        resultado.innerHTML += `<br>${key}: ${value}`;
-    }
-    fetch("https://jsonplaceholder.typicode.com/users", {
-        method: "POST",
-        body: JSON.stringify(contForm),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((res) => res.json())
-        .catch((error) => console.error("Error:", error))
-        .then((response) => console.log("Success:", response));
+
+formulario.addEventListener("submit", function(e){
+  e.preventDefault();
+  const contForm = new FormData(formulario);
+  fetch(URL, {
+      method: "POST",
+      body: JSON.stringify(contForm),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .catch((error) => console.error("Error:", error))
+      .then((response) => console.log("Success:", response));
 });
